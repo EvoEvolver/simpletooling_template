f85fbb519ba7c9808f22a66b8d03a6659e1ac93e
Zijian Zhang <doomspec@outlook.com>
2025-08-30 18:39:13 -0400

    use load_tool_from_module

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tools.toolset import toolset
-import tools.calculator
 
 if __name__ == '__main__':
     toolset.serve(host="0.0.0.0", port=8000)
--- tools/toolset.py
+++ tools/toolset.py
@@ -1,4 +1,9 @@
 from simpletooling import Toolset
+from simpletooling.indexing import load_tool_from_module
+
+import tools
 
 # Create a Toolset instance
-toolset = Toolset(title="Calculator API", version="1.0.0")
+toolset = Toolset(title="Template API", version="1.0.0")
+
+load_tool_from_module(tools)
